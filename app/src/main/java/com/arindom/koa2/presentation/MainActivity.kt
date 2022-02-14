25d2e444cf03bae8a90a8d4096e010495960c26d
arindom ghosh <arindomghosh23@gmail.com>
2022-02-15 04:29:02 +0530

    initial commit: arch setup

---
+++ app/src/main/java/com/arindom/koa2/presentation/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.arindom.koa2.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import com.arindom.koa2.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.main_activity)
